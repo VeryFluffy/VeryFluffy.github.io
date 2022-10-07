3784ee42de50107283e03055c41730e65b53cf46
fluffyhorse <114726084+VeryFluffy@users.noreply.github.com>
2022-10-07 09:13:43 +0300

    Update player.js

--- music/player.js
+++ music/player.js
@@ -3,7 +3,7 @@ function Lofi(e){
 if(blocking == false){
 blocking = true;
 var tracks = [];
-tracks[0] = ['Take-Me-Home.mp3', 'Take-Me-Home.mp3', 'Take-Me-Home.mp3'];
+tracks[0] = ['Take-Me-Home.mp3', 'Lost-in-Thoughts.mp3', 'Take-Me-Home.mp3'];
          var current = 0;
          if("pause" in audioN) audioN.pause();
          audioN = new Audio();
